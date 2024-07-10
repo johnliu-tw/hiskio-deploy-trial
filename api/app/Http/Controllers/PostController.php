1475b54f8ae028bd3d8dcd57de79619527d66206
johnliu <johnliutw@hotmail.com>
2024-07-10 23:22:47 +0800

    add storage

--- api/app/Http/Controllers/PostController.php
+++ api/app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -34,5 +35,28 @@ class PostController extends Controller
     {
         Post::findOrFail($id)->delete();
         return response()->json(null, 204);
+    }
+
+    public function latestReport()
+    {
+        $post = Post::where('is_published', true)
+            ->orderBy('published_at', 'desc')
+            ->first();
+
+        if (!$post) {
+            return response()->json(['message' => 'No published post found'], 404);
+        }
+
+        $content = "ID: {$post->id}\n";
+        $content .= "Title: {$post->title}\n";
+        $content .= "Content: {$post->content}\n";
+
+        $fileName = "latest_post_report.txt";
+
+        Storage::disk('public')->put($fileName, $content);
+
+        $fileUrl = Storage::disk('public')->url($fileName);
+
+        return response()->json(['file_url' => $fileUrl]);
     }
 }
--- api/routes/web.php
+++ api/routes/web.php
@@ -3,4 +3,5 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
+Route::get('/posts/latest-report', [PostController::class, 'latestReport']);
 Route::apiResource('posts', PostController::class);
